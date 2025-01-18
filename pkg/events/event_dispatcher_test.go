63b8b979a077284e61c500c2af1d48615137138a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-01-18 18:25:20 -0300

    feat: add initial event dispatcher test

---
+++ pkg/events/event_dispatcher_test.go
@@ -0,0 +1,39 @@
+package events
+
+import (
+  "time"
+  "github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+  Name string
+  Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+  return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+  return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+  return time.Now()
+}
+
+type TestEventHandler struct {}
+
+func (e *TestEventHandler) Handle(event EventInterface) {
+  // do something
+}
+
+type EventDispatcherTestSuite struct {
+  suite.Suite
+  event TestEvent
+  event2 TestEvent
+  handler TestEventHandler
+  handler2 TestEventHandler
+  handler3 TestEventHandler
+  eventDispatcher EventDispatcher
+}
