045d188e18a87f7700bd38ccd8d4796684a6822d
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-01-19 13:57:58 -0300

    feat: starting to create dependencies

---
+++ cmd/walletcore/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/higorrsc/fc-hrsc-eda/internal/database"
+	"github.com/higorrsc/fc-hrsc-eda/internal/event"
+	"github.com/higorrsc/fc-hrsc-eda/internal/usecase/create_account"
+	"github.com/higorrsc/fc-hrsc-eda/internal/usecase/create_client"
+	"github.com/higorrsc/fc-hrsc-eda/internal/usecase/create_transaction"
+	"github.com/higorrsc/fc-hrsc-eda/pkg/events"
+)
+
+func main() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	eventDispatcher := events.NewEventDispatcher()
+	transactionCreatedEvent := event.NewTransactionCreated()
+	// eventDispatcher.Register("TransactionCreated", handler)
+
+	clientDb := database.NewClientDB(db)
+	accountDb := database.NewAccountDB(db)
+	transactionDb := database.NewTransactionDB(db)
+
+	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
+	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+}
