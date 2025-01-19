abdcc6e12fb5d1f3671abc60207ef07d78180249
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-01-19 15:56:19 -0300

    feat: adjust tests to use mocks package

--- internal/usecase/create_account/create_account_test.go
+++ internal/usecase/create_account/create_account_test.go
@@ -4,44 +4,17 @@ import (
 	"testing"
 
 	"github.com/higorrsc/fc-hrsc-eda/internal/entity"
+	"github.com/higorrsc/fc-hrsc-eda/internal/usecase/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ClientGatewayMock struct {
-	mock.Mock
-}
-
-func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Client), args.Error(1)
-}
-
-func (m *ClientGatewayMock) Save(client *entity.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
-func (m *AccountGatewayMock) Save(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
 func TestCreateAccountUseCase_Execute(t *testing.T) {
 	client, _ := entity.NewClient("John Doe", "L4uQK@example.com")
-	clientMock := &ClientGatewayMock{}
+	clientMock := &mocks.ClientGatewayMock{}
 	clientMock.On("Get", client.ID).Return(client, nil)
 
-	accountMock := &AccountGatewayMock{}
+	accountMock := &mocks.AccountGatewayMock{}
 	accountMock.On("Save", mock.Anything).Return(nil)
 
 	uc := NewCreateAccountUseCase(accountMock, clientMock)
